e8f1a1db499d9f81557a00374411ffe28f015b0f
David Leal <david@davidleal.com>
2025-05-21 17:11:49 +0100

    Update .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,9 @@
+import Ecto.Query
+
 alias CMS.Repo
 alias CMS.Accounts.Organization
 alias CMS.Accounts.User
 alias CMS.Liturgies.Block
+alias CMS.Liturgies.Liturgy
 
 IEx.configure(inspect: [limit: :infinity])
