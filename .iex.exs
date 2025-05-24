2de04df1349562327e604a9d3c61857efca29e4d
David Leal <david@davidleal.com>
2025-05-24 14:49:30 +0100

    Add a few more modules to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -2,8 +2,11 @@ import Ecto.Query
 
 alias CMS.Repo
 alias CMS.Accounts.Organization
+alias CMS.Accounts.Scope
 alias CMS.Accounts.User
 alias CMS.Liturgies.Block
+alias CMS.Liturgies.Blocks
 alias CMS.Liturgies.Liturgy
+alias CMS.Liturgies.LiturgyBlock
 
 IEx.configure(inspect: [limit: :infinity])
