b0867948a8b417efc1974ef2c1a3959df41ece71
David Leal <david@davidleal.com>
2025-05-29 11:58:20 +0100

    Enable auto-reload (?)

--- .iex.exs
+++ .iex.exs
@@ -9,4 +9,7 @@ alias CMS.Liturgies.Blocks
 alias CMS.Liturgies.Liturgy
 alias CMS.Liturgies.LiturgyBlock
 
-IEx.configure(inspect: [charlists: :as_lists, limit: :infinity, structs: true])
+IEx.configure(
+  auto_reload: true,
+  inspect: [charlists: :as_lists, limit: :infinity, structs: true]
+)
