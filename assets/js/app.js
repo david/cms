b67ac27924fccb065d7003d4777561c5d659d91a
David Leal <david@davidleal.com>
2025-05-30 21:00:26 +0100

    PWA

--- assets/js/app.js
+++ assets/js/app.js
@@ -101,3 +101,15 @@ if (process.env.NODE_ENV === "development") {
     window.liveReloader = reloader
   })
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
