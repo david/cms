0196c3e6a3856d73fedc4c071a5dcbab73ff960e
David Leal <david@davidleal.com>
2025-06-07 07:40:39 +0000

    Remove docker-compose

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,6 @@ import Config
 
 # Configure your database
 config :cms, CMS.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "db",
   database: "cms_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
