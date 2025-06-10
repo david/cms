7952912c02e9d04105bf5642234ffbb46fe2aae2
David Leal <david@davidleal.com>
2025-06-10 19:23:36 +0000

    Remove redundant function

--- lib/cms/liturgies/block.ex
+++ lib/cms/liturgies/block.ex
@@ -31,12 +31,14 @@ defmodule CMS.Liturgies.Block do
 
   @doc false
   def changeset(block, attrs, index, liturgy_attrs, user_scope) do
+    type = get_type(block, attrs, liturgy_attrs, index)
+
     block
     |> cast(attrs, [:song_id, :position, :body, :subtitle, :title, :type])
-    |> put_change(:type, get_type(block, attrs, liturgy_attrs, index))
+    |> put_change(:type, type)
     |> put_change(:organization_id, user_scope.organization.id)
     |> put_change(:position, index)
-    |> put_block(attrs, index, liturgy_attrs, user_scope)
+    |> put_block(type, attrs, user_scope)
   end
 
   defp get_type(block, attrs, liturgy_attrs, index) do
@@ -50,17 +52,13 @@ defmodule CMS.Liturgies.Block do
       end
   end
 
-  defp put_block(changeset, attrs, index, liturgy_attrs, user_scope) do
-    build_song(changeset, get_field(changeset, :type), attrs, user_scope)
-  end
+  defp put_block(changeset, :text, _attrs, _scope), do: changeset
 
-  defp build_song(changeset, :text, _attrs, _scope), do: changeset
-
-  defp build_song(changeset, :passage, %{"title" => title}, _user_scope) do
+  defp put_block(changeset, :passage, %{"title" => title}, _user_scope) do
     put_change(changeset, :body, Bibles.get_verses(title))
   end
 
-  defp build_song(
+  defp put_block(
          %{data: %{song_id: song_id}} = changeset,
          :song,
          _attrs,
@@ -69,7 +67,7 @@ defmodule CMS.Liturgies.Block do
        when not is_nil(song_id),
        do: changeset
 
-  defp build_song(changeset, :song, %{"title" => title}, user_scope) do
+  defp put_block(changeset, :song, %{"title" => title}, user_scope) do
     song = Songs.suggest(user_scope, title)
 
     cond do
@@ -94,7 +92,7 @@ defmodule CMS.Liturgies.Block do
     end
   end
 
-  defp build_song(changeset, _type, _attrs, _user_scope), do: changeset
+  defp put_block(changeset, _type, _attrs, _user_scope), do: changeset
 
   def make_template(%{type: type, subtitle: subtitle}) do
     %__MODULE__{type: type, subtitle: subtitle}
