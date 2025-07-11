f50ca4004f854a79c1716dc1ee5cb7431cf52901
David Leal <david@davidleal.com>
2025-07-11 21:39:27 +0000

    Make PWA installable on iOS

--- priv/static/sw.js
+++ priv/static/sw.js
@@ -1,35 +1,62 @@
-self.addEventListener('install', function(e) {
-  // once the SW is installed, go ahead and cache the resources
-  e.waitUntil(
-    fetch('/cache_manifest.json')
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(cacheManifest) {
-        var cacheName = 'cache:static:' + cacheManifest.version
-        var all = Object.values(cacheManifest.latest).filter(
-          function(fn) { return fn.match(/^(images|css|js|fonts)/);
-          })
-        caches.open(cacheName).then(function(cache) {
-          return cache.addAll(all).then(function() {
-            self.skipWaiting();
-          });
-        })
-      })
+const CACHE_NAME = "v1";
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      // Setting {cache: 'reload'} in the new request will ensure that the response
+      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
   );
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
 });
 
-// when the browser fetches a url
-self.addEventListener('fetch', function(event) {
-  // either respond with the cached object fetch it
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        // retrieve from cache
-        return response;
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preloading if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preloading
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
       }
-      // fetch as normal
-      return fetch(event.request);
-    })
+    })()
   );
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is likely
+          // due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
 });
