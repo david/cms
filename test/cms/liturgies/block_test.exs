c517d63f7c7b4b30e62c9229d8e8138ffc0424cb
David Leal <david@davidleal.com>
2025-07-14 20:51:43 +0100

    [block] Test validation of block for existing song

--- test/cms/liturgies/block_test.exs
+++ test/cms/liturgies/block_test.exs
@@ -5,6 +5,7 @@ defmodule CMS.Liturgies.BlockTest do
 
   import Ecto.Changeset
   import CMS.AccountsFixtures
+  import CMS.SongsFixtures
 
   setup do
     %{scope: user_scope_fixture()}
@@ -39,6 +40,18 @@ defmodule CMS.Liturgies.BlockTest do
       assert get_field(changeset, :type) == :song
       assert get_field(changeset, :position) == 0
       assert get_field(changeset, :organization_id) == scope.organization.id
+    end
+
+    test "for a song that already exists", %{scope: scope} do
+      song = song_fixture(%{organization_id: scope.organization.id})
+      attrs = %{"title" => song.title}
+      liturgy_attrs = %{"blocks_sort" => ["new-song"]}
+
+      changeset = Block.changeset(%Block{}, attrs, 0, liturgy_attrs, scope)
+
+      assert changeset.valid?
+      assert get_change(changeset, :song).data.id == song.id
+      assert get_field(changeset, :body) == song.body
     end
   end
 
