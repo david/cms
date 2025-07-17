e15a2fca6be18c8626e9578ddb6c718ab6dcd6cc
David Leal <david@davidleal.com>
2025-07-17 15:37:33 +0100

    Try once more to make app installable on iOS

--- priv/static/sw.js
+++ priv/static/sw.js
@@ -1,62 +1,70 @@
-const CACHE_NAME = "v1";
+const CACHE_NAME = "v2";
 const OFFLINE_URL = "offline.html";
+const ASSETS = [
+  "/",
+  "/assets/css/app.css",
+  "/assets/js/app.js",
+  "/images/icons/icon-512x512.png",
+  OFFLINE_URL,
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request will ensure that the response
-      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+      await cache.addAll(ASSETS.map(url => new Request(url, { cache: "reload" })));
     })()
   );
-  // Force the waiting service worker to become the active service worker.
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     (async () => {
-      // Enable navigation preloading if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preloading
       if ("navigationPreload" in self.registration) {
         await self.registration.navigationPreload.enable();
       }
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })()
   );
-
-  // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
   if (event.request.mode === "navigate") {
     event.respondWith(
       (async () => {
         try {
-          // First, try to use the navigation preload response if it's supported.
           const preloadResponse = await event.preloadResponse;
           if (preloadResponse) {
             return preloadResponse;
           }
-
-          // Always try the network first.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
+          return await fetch(event.request);
         } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
           console.log("Fetch failed; returning offline page instead.", error);
-
           const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+          return await cache.match(OFFLINE_URL);
         }
       })()
     );
+    return;
   }
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })()
+  );
 });
